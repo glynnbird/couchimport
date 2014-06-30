88afa493a46b17cf06a7b52b0b9481d161ffea50
Glynn Bird <glynn@scoot.co.uk>
2014-06-30 15:56:30 +0100

    fixes

--- includes/config.js
+++ includes/config.js
@@ -1,19 +1,34 @@
 var theconfig = {};
 
-// die if COUCH_URL or COUCHDB_DATABASE is unknown
-if (typeof process.env.COUCH_URL == "undefined" || typeof process.env.COUCH_DATABASE == "undefined") {
-  console.log("Requires environment variables COUCH_URL && COUCH_DATABASE")
-  process.exit(1);
+// configure the CouchDB paramss
+theconfig.COUCH_URL = "http://localhost:5984";
+theconfig.COUCH_DATABASE = "test";
+theconfig.COUCH_TRANSFORM = null;
+theconfig.COUCH_DELIMETER = "\t";
+
+// if we have a custom CouchDB url
+if( typeof process.env.COUCH_URL != "undefined") {
+  theconfig.COUCH_URL = process.env.COUCH_URL;
 }
 
-// configure the CouchDB paramss
-theconfig.COUCH_URL = process.env.COUCH_URL;
-theconfig.COUCH_DATABASE = process.env.COUCH_DATABASE;
-theconfig.COUCH_TRANSFORM = null;
+// if we have a custom CouchDB url
+if( typeof process.env.COUCH_DATABASE != "undefined") {
+  theconfig.COUCH_DATABASE = process.env.COUCH_DATABASE;
+}
 
 // if we have a customised transformation function
 if( typeof process.env.COUCH_TRANSFORM != "undefined") {
   theconfig.COUCH_TRANSFORM = require(process.env.COUCH_TRANSFORM)
 }
+
+// if we have overridden the delimeter field
+if( typeof process.env.COUCH_DELIMETER != "undefined") {
+  theconfig.COUCH_DELIMETER = process.env.COUCH_DELIMETER;
+}
+
+console.log("******************");
+console.log(" COUCHIMPORT - configuration")
+console.log("  ", JSON.stringify(theconfig, null, true).replace(/\/\/.+@/g, "//****:****@"));
+console.log("******************")
 
 module.exports = theconfig;
