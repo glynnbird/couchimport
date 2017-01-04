2df706a31ac5c00f5126fba518c99d945719df31
assafmo <assaf.morami@gmail.com>
2017-01-04 19:09:44 +0200

    support jsonl

--- app.js
+++ app.js
@@ -10,24 +10,30 @@ var fs = require('fs'),
 // rs - readable stream
 // opts - an options object, or null for defaults
 // callback - called when complete
-var importStream = function(rs, opts, callback) {
-  
+var importStream = function (rs, opts, callback) {
+
   // sort the paramters
   if (typeof callback == "undefined" && typeof opts == "function") {
     callback = opts;
     opts = null;
   }
-  
+
   // merge default options
   opts = defaults.merge(opts);
 
   // load dependencies
   var writer = require('./includes/writer.js')(opts.COUCH_URL, opts.COUCH_DATABASE, opts.COUCH_BUFFER_SIZE, opts.COUCH_PARALLELISM, opts.COUCH_IGNORE_FIELDS),
-      transformer = require('./includes/transformer.js')(opts.COUCH_TRANSFORM, opts.COUCH_META);
-  
-  // if this is a JSON stream
-  if (opts.COUCH_FILETYPE == "json") {
-    
+    transformer = require('./includes/transformer.js')(opts.COUCH_TRANSFORM, opts.COUCH_META);
+
+  if (opts.COUCH_FILETYPE == "jsonl") {
+    // pipe the file to a streaming JSON parser
+    var JSONStream = require('JSONStream');
+    rs.pipe(JSONStream.parse())
+      .pipe(transformer)  // process each object
+      .pipe(writer); // write the data
+  }
+  else if (opts.COUCH_FILETYPE == "json") {
+    // if this is a JSON stream
     if (!opts.COUCH_JSON_PATH) {
       var msg = "ERROR: you must specify a JSON path using --jsonpath or COUCH_JSON_PATH";
       debugimport(msg);
@@ -38,29 +44,29 @@ var importStream = function(rs, opts, callback) {
     rs.pipe(JSONStream.parse(opts.COUCH_JSON_PATH))
       .pipe(transformer)  // process each object
       .pipe(writer); // write the data
-      
+
   } else {
 
     // load the CSV parser
     var parse = require('csv-parse'),
-      objectifier = parse({delimiter: opts.COUCH_DELIMITER, columns: true, skip_empty_lines: true, relax: true});
-    
+      objectifier = parse({ delimiter: opts.COUCH_DELIMITER, columns: true, skip_empty_lines: true, relax: true });
+
     // pipe the input to the output, via transformation functions
     rs.pipe(objectifier)  // turn each line into an object
       .pipe(transformer)  // process each object
       .pipe(writer); // write the data
   }
-  
-  writer.on('writecomplete', function(data) {
+
+  writer.on('writecomplete', function (data) {
     debugimport('writecomplete', data);
     callback(null, data);
   });
-  
-  rs.on('error', function(e) {
+
+  rs.on('error', function (e) {
     debugimport('error', e);
     callback(e, null);
   });
-  
+
   return writer;
 };
 
@@ -68,12 +74,12 @@ var importStream = function(rs, opts, callback) {
 // filename - name of the file stream
 // opts - an options object, or null for defaults
 // callback - called when complete
-var importFile = function(filename, opts, callback) {
+var importFile = function (filename, opts, callback) {
   return importStream(fs.createReadStream(filename), opts, callback);
 };
 
-var strip = function(str) {
-  return str.replace(/\n/g,' ').replace(/\r/g,'');
+var strip = function (str) {
+  return str.replace(/\n/g, ' ').replace(/\r/g, '');
 }
 
 // export to a writable stream
@@ -82,41 +88,41 @@ var strip = function(str) {
 // callback - called when complete
 var exportStream = function (ws, opts, callback) {
   var escape = null;
-  
+
   // sort the paramters
   if (typeof callback == "undefined" && typeof opts == "function") {
     callback = opts;
     opts = null;
   }
-  
+
   // merge default options
   opts = defaults.merge(opts);
-  
+
   var total = 0,
     headings = [],
     lastsize = 0,
     reader = require('./includes/reader.js')(opts.COUCH_URL, opts.COUCH_DATABASE, opts.COUCH_BUFFER_SIZE);
 
   // export a row as a CSV
-  var exportAsCSV = function(row) {
-  
+  var exportAsCSV = function (row) {
+
     // ignore design docs
     if (row._id.match(/^_design/)) {
       return;
     }
-  
+
     // if we are extracting headings
     if (headings.length == 0) {
       headings = Object.keys(row);
-      opts.COUCH_IGNORE_FIELDS.forEach(function(f) {
+      opts.COUCH_IGNORE_FIELDS.forEach(function (f) {
         headings = _.without(headings, f);
       });
       ws.write(headings.join(opts.COUCH_DELIMITER) + "\n");
     }
-  
+
     // output columns
     var cols = [];
-    for(var i in headings) {
+    for (var i in headings) {
       var v = row[headings[i]];
       var t = typeof v;
       if (v == null) {
@@ -132,9 +138,9 @@ var exportStream = function (ws, opts, callback) {
     ws.write(cols.join(opts.COUCH_DELIMITER) + "\n");
   }
 
-  async.doUntil(function(callback){
-    reader(function(err, data) {
-      if(err) {
+  async.doUntil(function (callback) {
+    reader(function (err, data) {
+      if (err) {
         return callback(true);
       }
       lastsize = data.length;
@@ -146,25 +152,25 @@ var exportStream = function (ws, opts, callback) {
       callback(null);
     });
   },
-  function() {
-    return (lastsize == 0 || escape);
-  }, 
-  function(err){
-    debugexport("Output complete");
-    callback(escape, null);
-  });
-  
-  ws.on("error", function(err) {
+    function () {
+      return (lastsize == 0 || escape);
+    },
+    function (err) {
+      debugexport("Output complete");
+      callback(escape, null);
+    });
+
+  ws.on("error", function (err) {
     escape = err;
   });
-  
+
 };
 
 // export to a named file 
 // filename - name of the file stream
 // opts - an options object, or null for defaults
 // callback - called when complete
-var exportFile = function(filename, opts, callback) {
+var exportFile = function (filename, opts, callback) {
   exportStream(fs.createWriteStream(filename), opts, callback);
 };
 
@@ -179,14 +185,14 @@ var previewCSVFile = preview.file;
 // opts - an options object, or null for defaults
 // callback - called when complete
 var previewURL = preview.url;
- 
+
 
 // load the first 10k of a URL and parse the first 3 lines
 // URL - name of the file to load
 // opts - an options object, or null for defaults
 // callback - called when complete
 var previewStream = preview.stream;
- 
+
 module.exports = {
   importStream: importStream,
   importFile: importFile,
--- includes/config.js
+++ includes/config.js
@@ -5,95 +5,95 @@ var defaults = require('./defaults.js'),
   argv = require('minimist')(process.argv.slice(2));
 
 // configure the CouchDB params
-var types = ["text","json"];
+var types = ["text", "json", "jsonl"];
 
 // if we have a custom CouchDB url
-if( typeof process.env.COUCH_URL != "undefined") {
+if (typeof process.env.COUCH_URL != "undefined") {
   theconfig.COUCH_URL = process.env.COUCH_URL;
 }
 
 // if we have a custom CouchDB url
-if( typeof process.env.COUCH_DATABASE != "undefined") {
+if (typeof process.env.COUCH_DATABASE != "undefined") {
   theconfig.COUCH_DATABASE = process.env.COUCH_DATABASE;
 }
 
 // if we have a customised transformation function
-if( typeof process.env.COUCH_TRANSFORM != "undefined") {
-  theconfig.COUCH_TRANSFORM = require(path.resolve(process.cwd(),process.env.COUCH_TRANSFORM));
+if (typeof process.env.COUCH_TRANSFORM != "undefined") {
+  theconfig.COUCH_TRANSFORM = require(path.resolve(process.cwd(), process.env.COUCH_TRANSFORM));
 }
 
 // if we have overridden the delimeter field
-if( typeof process.env.COUCH_DELIMITER != "undefined") {
+if (typeof process.env.COUCH_DELIMITER != "undefined") {
   theconfig.COUCH_DELIMITER = process.env.COUCH_DELIMITER;
 }
 
 // if there is metadata specified
-if( typeof process.env.COUCHIMPORT_META != "undefined") {
+if (typeof process.env.COUCHIMPORT_META != "undefined") {
   theconfig.COUCHIMPORT_META = JSON.parse(process.env.COUCHIMPORT_META);
 }
 
 // if there is type specified
-if( typeof process.env.COUCH_FILETYPE == "string" && types.indexOf(process.env.COUCH_FILETYPE)!=-1) {
+if (typeof process.env.COUCH_FILETYPE == "string" && types.indexOf(process.env.COUCH_FILETYPE) != -1) {
   theconfig.COUCH_FILETYPE = "json";
 }
 
 // if there is a buffer size specified
-if( typeof process.env.COUCH_BUFFER_SIZE != "undefined") {
+if (typeof process.env.COUCH_BUFFER_SIZE != "undefined") {
   theconfig.COUCH_BUFFER_SIZE = parseInt(process.env.COUCH_BUFFER_SIZE);
 }
 
 // if there is a buffer size specified
-if( typeof process.env.COUCH_JSON_PATH != "undefined") {
+if (typeof process.env.COUCH_JSON_PATH != "undefined") {
   theconfig.COUCH_JSON_PATH = process.env.COUCH_JSON_PATH;
 }
 
 // if there is a parallelism specified
-if( typeof process.env.COUCH_PARALLELISM != "undefined") {
+if (typeof process.env.COUCH_PARALLELISM != "undefined") {
   theconfig.COUCH_PARALLELISM = parseInt(process.env.COUCH_PARALLELISM);
 }
 
 // if this is preview mode
-if( typeof process.env.COUCH_PREVIEW != "undefined") {
+if (typeof process.env.COUCH_PREVIEW != "undefined") {
   theconfig.COUCH_PREVIEW = true;
 }
 
 // list of field names to ignore
-if( typeof process.env.COUCH_IGNORE_FIELDS != "undefined") {
+if (typeof process.env.COUCH_IGNORE_FIELDS != "undefined") {
   theconfig.COUCH_IGNORE_FIELDS = process.env.COUCH_IGNORE_FIELDS.split(',');
 }
 
 // override with command-line parameters
-if(argv.url) {
+if (argv.url) {
   theconfig.COUCH_URL = argv.url;
 }
-if(argv.db) {
+if (argv.db) {
   theconfig.COUCH_DATABASE = argv.db;
 }
-if(argv.transform) {
-  theconfig.COUCH_TRANSFORM = require(path.resolve(process.cwd(),argv.transform))
+if (argv.transform) {
+  theconfig.COUCH_TRANSFORM = require(path.resolve(process.cwd(), argv.transform))
 }
-if(argv.delimiter) {
+if (argv.delimiter) {
   theconfig.COUCH_DELIMITER = argv.delimiter;
 }
-if(argv.meta) {
+if (argv.meta) {
   theconfig.COUCHIMPORT_META = JSON.parse(argv.meta);
 }
-if(argv.type && types.indexOf(argv.type)!=-1) {
+if (argv.type && types.indexOf(argv.type) != -1) {
   theconfig.COUCH_FILETYPE = argv.type;
 }
-if(argv.buffer) {
+if (argv.buffer) {
   theconfig.COUCH_BUFFER_SIZE = parseInt(argv.buffer);
 }
-if(argv.jsonpath) {
+if (argv.jsonpath) {
   theconfig.COUCH_JSON_PATH = argv.jsonpath;
 }
-if(argv.parallelism) {
+if (argv.parallelism) {
   theconfig.COUCH_PARALLELISM = parseInt(argv.parallelism);
 }
-if(argv.preview) {
+if (argv.preview) {
   theconfig.COUCH_PREVIEW = true;
 }
-if(argv.ignorefields) {
+if (argv.ignorefields) {
   theconfig.COUCH_IGNORE_FIELDS = argv.ignorefields.split(',');
 }
 
