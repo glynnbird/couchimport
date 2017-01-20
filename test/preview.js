6db934e7276039d2cdde0a6d3998506698891224
Glynn Bird <glynn.bird@gmail.com>
2017-01-20 14:48:12 +0000

    Allow preview mode to do transforms. fixes issue #120

--- test/preview.js
+++ test/preview.js
@@ -10,27 +10,43 @@ describe('Preview mode', function() {
   it('preview csv', function(done) {
     couchimport.previewCSVFile("./test/test.csv", opts, function(err,data, delimiter) {
       assert.equal(err, null);
-      data.should.be.an.Object;
+      data.should.be.an.Array;
       delimiter.should.be.a.String;
       delimiter.should.equal(",");
       done();
     });
   });
 
-  if('preview tsv', function(done) {
-    couchimport.previewCSVFile("./test/guitars.csv", opts, function(err,data, delimiter) {
+  it('preview tsv', function(done) {
+    couchimport.previewCSVFile("./test/guitars.tsv", opts, function(err,data, delimiter) {
       assert.equal(err, null);
-      data.should.be.an.Object;
+      data.should.be.an.Array;
       delimiter.should.be.a.String;
       delimiter.should.equal("\t");
       done();
     });
   })
 
-  if('preview nonsense', function(done) {
-    couchimport.previewCSVFile("./test/guitars.csv", opts, function(err,data, delimiter) {
+  it('preview tsv with transform', function(done) {
+    opts.COUCH_TRANSFORM = function(doc) {
+      doc.price = parseFloat(doc.price);
+      return doc;
+    };
+    couchimport.previewCSVFile("./test/guitars.tsv", opts, function(err,data, delimiter) {
       assert.equal(err, null);
-      data.should.be.an.Object;
+      data.should.be.an.Array;
+      data[0].should.be.an.Object;
+      data[0].price.should.be.a.Number;
+      delimiter.should.be.a.String;
+      delimiter.should.equal("\t");
+      done();
+    });
+  })
+
+  it('preview nonsense', function(done) {
+    couchimport.previewCSVFile("./test/guitars.tsv", opts, function(err,data, delimiter) {
+      assert.equal(err, null);
+      data.should.be.an.Array;
       delimiter.should.be.a.String;
       delimiter.should.equal("\t");
       done();
