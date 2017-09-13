429283088556511f69ac7fefdbceaf6763cb9965
Gregor <gregor@martynus.net>
2017-09-13 13:10:25 -0700

    feat: allow transform function to return array of docs

--- includes/transformer.js
+++ includes/transformer.js
@@ -1,18 +1,27 @@
 var stream = require('stream');
 
 module.exports = function(func, meta) {
-  
+
   var transformer = new stream.Transform( { objectMode: true } );
   transformer._transform = function (obj, encoding, done) {
+    var self = this
+
     // transform using custom function
     if(typeof func == "function") {
       obj = func(obj, meta);
     }
 
     // pass object to next stream handler
-    this.push( obj);
+    if (Array.isArray(obj)) {
+      obj.forEach(function (obj) {
+        self.push( obj)
+      })
+    } else {
+      this.push( obj);
+    }
+
     done();
   };
-  
+
   return transformer;
 }
